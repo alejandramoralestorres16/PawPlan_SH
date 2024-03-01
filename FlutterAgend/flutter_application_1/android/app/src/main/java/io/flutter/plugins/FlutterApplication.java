9161517d0f564b47e3290a9accd5246178329d68
alejandramoralestorres16 <126217488+alejandramoralestorres16@users.noreply.github.com>
2024-03-01 17:38:37 -0600

    Add files via upload

---
+++ FlutterAgend/flutter_application_1/android/app/src/main/java/io/flutter/plugins/FlutterApplication.java
@@ -0,0 +1,11 @@
+package io.flutter.app;
+
+import androidx.multidex.MultiDexApplication;
+
+public class FlutterApplication extends MultiDexApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Aquí puedes agregar cualquier inicialización adicional si es necesario
+    }
+}
