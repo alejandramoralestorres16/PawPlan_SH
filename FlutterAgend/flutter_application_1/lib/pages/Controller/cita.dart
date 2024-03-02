ac36eae50285d7da0904cb0cbcd31adca6d1f5c3
alondraReyna11 <126124032+alondraReyna11@users.noreply.github.com>
2024-03-01 19:19:13 -0600

    Update cita.dart

--- FlutterAgend/flutter_application_1/lib/pages/Controller/cita.dart
+++ FlutterAgend/flutter_application_1/lib/pages/Controller/cita.dart
@@ -3,14 +3,4 @@ import 'package:table_calendar/table_calendar.dart';
 import 'agendC.dart';
 import '../Model/agenda.dart';
 
-class Cita {
-  final String titulo;
-  final String descripcion;
-  final DateTime fecha;
 
-  Cita({
-    required this.titulo,
-    required this.descripcion,
-    required this.fecha,
-  });
-}
